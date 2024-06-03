0ef1e8c04a58ce33fc68e614f80dba4b7d4d5e00
Notjessand <jessennorom@gmail.com>
2024-06-03 13:39:46 -0500

    news

---
+++ components/nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="flex justify-between py-4 sm:py-10 px-10 bg-slate-950 text-white mb-4">
+      {" "}
+      <div className="h-9 px-6 rounded text-3xl font-display">Thugger News</div>
+      <div className="h-9 w-24 bg-slate-500 rounded"></div>
+    </div>
+  );
+};
+
+export default Nav;
